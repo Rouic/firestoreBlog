a84a9ec89d9a4f207636026486d5ef6d3f94aeea
Alex Cottenham <alex@rouic.com>
2021-02-27 18:32:22 +0000

    Update build configurations

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import 'bootstrap';
+
+import angular from 'angular';
+import uiRouter from '@uirouter/angularjs';
+import * as ngAnimate from 'angular-animate';
+import ngCookies from 'angular-cookies';
+
+
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/performance';
+import 'firebase/analytics';
+import 'firebase/firestore';
+import 'firebase/database';
+
+/* Firebase config start */
+const firebaseConfig = {
+  apiKey: "AIzaSyAWrtYN72-U5Oby_W6UUQHpdtLoymDMOFk",
+  authDomain: "fb-firestore-blog.firebaseapp.com",
+  databaseURL: "https://fb-firestore-blog-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "fb-firestore-blog",
+  storageBucket: "fb-firestore-blog.appspot.com",
+  messagingSenderId: "992015533751",
+  appId: "1:992015533751:web:dfdb6c81d67fb9c156af32",
+  measurementId: "G-78QY8NCCDJ"
+};
+firebase.default.initializeApp(firebaseConfig);
+global.realTimeDatabase = firebase.default.database();
+global.perf = firebase.default.performance();
+global.analytics = firebase.default.analytics();
+global.db = firebase.default.firestore();
+global.auth = firebase.default.auth();
+global.firebase = firebase.default;
+/* Firebase config end */	  
+
+global.firestoreBlog = angular.module('firestoreBlog', [uiRouter, ngCookies, ngAnimate]);
+
+import './app/styles/style.scss';
+
---
+++ webpack.config.js
@@ -0,0 +1,174 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const glob = require('glob');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const PurgecssPlugin = require('purgecss-webpack-plugin');
+
+const PATHS = {
+	  src: path.join(__dirname, 'src')
+};
+
+module.exports = {
+  mode: 'development',
+  entry: {
+	main: './src/index.js',
+	app: './src/app/app.js'
+  },
+  output: {
+	filename: '[name].[contenthash:8].js',
+	path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+	runtimeChunk: 'single',
+	splitChunks: {
+	  chunks: 'all',
+	  maxInitialRequests: Infinity,
+	  minSize: 0,
+	  cacheGroups: {
+		  styles: {
+				name: 'styles',
+				test: /\.css$/,
+				chunks: 'all',
+				enforce: true
+			  },
+		vendor: {
+		  test: /[\\/]node_modules[\\/]/,
+		  name(module) {
+			const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+			return `npm.${packageName.replace('@', '')}`;
+		  },
+		},
+	  },
+	},
+  },
+  plugins: [
+	new HtmlWebpackPlugin({
+		template: './src/app/index.html',
+	}),
+	new MiniCssExtractPlugin({
+	  filename: 'style.[contenthash:8].css',
+	  chunkFilename: 'style.[contenthash:8].css',
+	}),
+	new PurgecssPlugin({
+		  paths: glob.sync(`${PATHS.src}/**/*`,  { nodir: true }),
+		  safelist: {
+			  greedy: [/md/]
+		}
+	}),
+	new CleanWebpackPlugin(),
+	new webpack.ProvidePlugin({
+	  jQuery: 'jquery',
+	  $: 'jquery',
+	  jquery: 'jquery',
+	  "window.jQuery": "jquery"
+	}),
+	// new CopyPlugin({
+	//   patterns: [
+	// 	{
+	// 	  from: '**',
+	// 	  to: 'assets/img/',
+	// 	  context: './src/assets/img/'
+	// 	}
+	//   ],
+	// }),
+  ],
+  module: {
+	rules: [
+		{
+		  test: /\.html$/,
+		  exclude: /index\.html$/,
+		  use: [
+			  {
+				  loader: 'file-loader',
+				  options: {
+					  name: 'template.[contenthash:8].html',
+					  outputPath: 'templates/'
+				  }
+			  },
+			  'extract-loader',
+			  {
+			  loader: 'html-loader',
+			  options: {
+				  attributes: false
+			  }
+			 
+				}
+		  ],
+		},
+		{
+			  test: /\.(scss)$/,
+			  use: [{
+				// inject CSS to page
+				loader: 'style-loader'
+			  }, {
+				// translates CSS into CommonJS modules
+				loader: 'css-loader'
+			  }, {
+				// Run postcss actions
+				loader: 'postcss-loader',
+				options: {
+				  // `postcssOptions` is needed for postcss 8.x;
+				  // if you use postcss 7.x skip the key
+				  postcssOptions: {
+					// postcss plugins, can be exported to postcss.config.js
+					plugins: function () {
+					  return [
+						require('autoprefixer')
+					  ];
+					}
+				  }
+				}
+			  }, {
+				// compiles Sass to CSS
+				loader: 'sass-loader'
+			  }]
+			},
+		{
+		test: /\.css$/,
+		use: [{
+			loader: MiniCssExtractPlugin.loader,
+			options: {
+			  publicPath: '/',
+			},
+		  },
+		  {
+		  loader: 'css-loader',
+			}
+		],
+	  },
+	  {
+		test: /\.(png|jpg|gif)$/i,
+		use: [{
+		  loader: 'url-loader',
+		  options: {
+			outputPath: 'assets/img/',
+			limit: 8192,
+		  },
+		}, ],
+	  },
+	  {
+		test: /\.svg$/i,
+		use: [{
+		  loader: 'svg-url-loader',
+		  options: {
+			outputPath: 'assets/img/',
+			limit: 10000,
+		  },
+		}, ],
+	  },
+	  {
+		test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+		use: [{
+		  loader: 'file-loader',
+		  options: {
+			name: '[name].[ext]',
+			outputPath: 'assets/fonts/'
+		  }
+		}]
+	  }
+	],
+  },
+};
